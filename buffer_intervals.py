6f7fa7138f6aa6a7540a9f85bff05cfbc483b126
ChrisPyatt <christopher.pyatt1@nhs.net>
2025-02-13 12:42:48 +0000

    refactor with dataframes and make them quick

--- buffer_intervals.py
+++ buffer_intervals.py
@@ -9,6 +9,7 @@ Script to iteratively split, merge, & buffer an intervals BED file to conform to
 import sys
 import subprocess
 import pandas as pd
+import time
 
 try:
     INFILE = sys.argv[1]
@@ -19,10 +20,10 @@ except:
 	print("----------\nUsage: python3 buffer_intervals.py INFILE INSERT_SIZE UPPER_LIMIT OUTFILE_PREFIX\n----------\n")
 
 
-def split_interval(chrom, start, end):
+def split_interval(chrom, start, end, buffer, upper):
     length = end - start
-    num_intervals = round(length / UPPER_LIMIT)
-    if length % UPPER_LIMIT < (INSERT_SIZE * 2):
+    num_intervals = round(length / upper)
+    if length % upper < (buffer * 2):
         num_intervals = num_intervals + 1
     # prevent infinite loop where num_intervals is 1 even though length > upper
     if num_intervals == 1:
@@ -33,23 +34,21 @@ def split_interval(chrom, start, end):
         interval_start = intervals[-1][1] + 1
         interval_end = interval_start + boundary
         intervals.append((interval_start, interval_end))
-    df = pd.DataFrame(intervals, columns=['buffered_start', 'buffered_end'])
+    df = pd.DataFrame(intervals, columns=['start', 'end'])
     df['chrom'] = chrom
-    df['start'] = None
-    df['end'] = None
     return df
 
 
 def merge_overlaps():
     # merge overlapping intervals
-    command="bedtools merge -d -1 -i targets.bed > merged.bed"
+    command = "bedtools merge -d -1 -i targets.bed > merged.bed"
     subprocess.run(command, shell=True)
 
 
 def write_targets_bed(content):
     # write amended data to intermediate bed file
      with open("targets.bed", "w") as fh:
-        content.to_csv(fh, sep="\t", header=False, columns=['chrom', 'buffered_start', 'buffered_end'])
+        content.to_csv(fh, sep="\t", header=False, index=False, columns=['chrom', 'start', 'end'])
 
 
 def clean_temp_files():
@@ -57,35 +56,37 @@ def clean_temp_files():
      subprocess.run("rm targets.bed; rm merged.bed", shell=True)
      
 
-def split_and_buffer(infile, buffer, upper):
+def buffer_intervals(infile, buffer):
     # read in bed file
-    with open(infile, 'r') as fh:
-        df = pd.read_csv(fh, sep="\t", header=None, names=["chrom", "start", "end"])
+    df = pd.read_csv(infile, sep="\t", header=None, names=["chrom", "start", "end"])
     df['chrom'] = df['chrom'].astype(str)
     # buffer all intervals
     if buffer < 1:
         buffer = 1
-    df['buffered_start'] = df['start'] - buffer
-    df['buffered_end'] = df['end'] + buffer
-    # split larger intervals
-    df_long = df[df['buffered_end'] - df['buffered_start'] > upper]
-    df_ok = df[df['buffered_end'] - df['buffered_start'] <= upper]
-    split_intervals = df_long.apply(lambda x: split_interval(x.chrom, x.start, x.end), axis=1)
-    # add split intervals back onto those below upper limit
-    print('commencing interval loop')
-    for interval_df in split_intervals:
-        print(interval_df['buffered_start'][0])
-        df_ok = pd.concat([df_ok, interval_df])
+    df['start'] = df['start'] - buffer
+    df['end'] = df['end'] + buffer
     # sort the dataframe & reset indices
-    df_ok = df_ok.sort_values(['chrom', 'buffered_start', 'buffered_end']).reset_index().drop('index', axis=1)
+    df = df.sort_values(['chrom', 'start', 'end']).reset_index(drop=True)
+    # return sorted dataframe
+    return df
+
+
+def split_intervals(df, buffer, upper):
+    # split larger intervals
+    df_long = df[df['end'] - df['start'] > upper]
+    df_ok = df[df['end'] - df['start'] <= upper]
+    df_split = pd.concat(df_long.apply(lambda x: split_interval(x.chrom, x.start, x.end, buffer, upper), axis=1).tolist())
+    # add split intervals back onto those below upper limit
+    df_ok = pd.concat([df_ok, df_split])
+    # sort the dataframe & reset indices
+    df_ok = df_ok.sort_values(['chrom', 'start', 'end']).reset_index(drop=True)
     # return sorted dataframe
     return df_ok
 
 
 def get_stats(bed_file):
     # make df
-    with open(bed_file, "r") as fh:
-        df = pd.read_csv(fh, sep="\t", header=None)
+    df = pd.read_csv(bed_file, sep="\t", header=None)
     # make length column
     df[3] = df[2] - df[1]
     # calculate stats
@@ -102,7 +103,7 @@ def main():
     # print stats of input file
     stats = get_stats(INFILE)
     print(
-        "Input file stats:\n"
+        "\nInput file stats:\n"
         f"\tMin: {stats[0]}\n"
         f"\tMax: {stats[1]}\n"
         f"\tMean: {stats[2]}\n"
@@ -110,20 +111,30 @@ def main():
         f"\tStd Deviation: {stats[4]}\n"
     )
     # round 1
-    file_content = split_and_buffer(INFILE, INSERT_SIZE, UPPER_LIMIT)
+    start = time.time()
+    buffered_intervals = buffer_intervals(INFILE, INSERT_SIZE)
+    file_content = split_intervals(buffered_intervals, INSERT_SIZE, UPPER_LIMIT)
     write_targets_bed(file_content)
     merge_overlaps()
+    end = time.time()
+    elapsed = "{:.2f}".format(end - start)
+    print(f"Time elapsed iteration 1: {elapsed} seconds")
     # iterate if needed
-    count = 0
-    while ( get_stats("merged.bed")[1] > UPPER_LIMIT ):
-        file_content = split_and_buffer("merged.bed", INSERT_SIZE, UPPER_LIMIT)
+    count = 1
+    while (get_stats("merged.bed")[1] > UPPER_LIMIT):
+        start = time.time()
+        df_merged = pd.read_csv("merged.bed", sep="\t", header=None, names=["chrom", "start", "end"])
+        file_content = split_intervals(df_merged, INSERT_SIZE, UPPER_LIMIT)
         write_targets_bed(file_content)
         merge_overlaps()
         count = count + 1
+        end = time.time()
+        elapsed = "{:.2f}".format(end - start)
+        print(f"Time elapsed iteration {count}: {elapsed} seconds")
     # report final output stats
     stats = get_stats("merged.bed")
     print(
-        "Output file stats:\n"
+        "\nOutput file stats:\n"
         f"\tMin: {stats[0]}\n"
         f"\tMax: {stats[1]}\n"
         f"\tMean: {stats[2]}\n"
